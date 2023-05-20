3e5b2ab68b84346c45d9709eb73bc18ae512d69e
Nick Amato <47062941+namato18@users.noreply.github.com>
2023-05-20 16:48:31 -0600

    Add files via upload

---
+++ DogeCoinML.R
@@ -0,0 +1,342 @@
+library(stringr)
+library(lubridate)
+library(xgboost)
+library(quantmod)
+library(caret)
+library(riingo)
+library(usethis)
+
+readRenviron(".Renviron")
+
+createModel <- function(TargetIncreasePercent = 4.5, SuccessThreshold = 0.3, Symbol, Timeframe, TP, SL){
+# 
+# # Riingo get data
+# df = riingo_crypto_prices(Symbol, end_date = Sys.Date(), resample_frequency = Timeframe)
+# # Testing quantmod
+# # df = data.frame(getSymbols(Symbol,
+# #            from = '2010-01-01',
+# #            to = Sys.Date(),
+# #            warnings = FALSE,
+# #            auto.assign = FALSE))
+# # 
+# # df = na.omit(df)
+# 
+# # Modify for Riingo
+# df$Percent.Change = NA
+# df = df[-1,-c(1:3,10:11)]
+# colnames(df) = c("Date","Open","High","Low","Close","Volume","Percent.Change")
+# df$Percent.Change = round((((df$Close / df$Open) * 100) - 100), digits = 1)
+# 
+# 
+# # Modify data to be more useable
+# # df$Date = row.names(df)
+# # df$Percent = NA
+# # df = df[,-c(5:6)]
+# # df = df[,c(5:4,1:3,6)]
+# # colnames(df) = c("Date","Close","Open","High","Low","Percent.Change")
+# # df$Percent.Change = round((((df$Close / df$Open) * 100) - 100), digits = 0)
+# 
+# 
+# 
+# 
+# # Add column for binary previouos day change
+# df$Previous = NA
+# for(i in 2:nrow(df)){
+#   if(df$Percent.Change[i - 1] <= 0){
+#     df$Previous[i] = 0
+#   }else{
+#     df$Previous[i] = 1
+#   }
+# }
+# 
+# # Remove first row since we can't use it
+# df = df[-1,]
+# 
+# 
+# # Round columns to be more general
+# df$Close = round(df$Close, digits = 3)
+# df$Open = round(df$Open, digits = 3)
+# df$High = round(df$High, digits = 3)
+# df$Low = round(df$Low, digits = 3)
+# df$Volume = round(df$Volume, digits=0)
+# 
+# 
+# # Convert to actual dates and remove year and change to numeric
+# df$Date = as.Date(df$Date)
+# df$Date = str_match(string = df$Date, pattern = "-(.*)")[,2]
+# df$Date = str_replace(df$Date, pattern = "-.*", replacement = "")
+# df$Date = as.numeric(df$Date)
+# 
+# # outcomes
+# outcome = rep(NA, nrow(df))
+# for(i in 1:length(outcome)-1){
+#   if(df$Percent.Change[i + 1] >= TargetIncreasePercent){
+#     outcome[i] = 1
+#   }else{
+#     outcome[i] = 0
+#   }
+# }
+# 
+# 
+# # Remove last row from df since we can't use it
+# outcome = outcome[-(length(outcome))]
+# df = df[-(nrow(df)),]
+# 
+# 
+# # Remove Previous column for testing
+# # df = df[,-ncol(df)]
+# 
+# # One hot encode everything
+# 
+# # df[,c(1:7)] = lapply(df[,c(1:7)], factor)
+# 
+# # remove some variables
+# # df = df[,-c(2,4,5)]
+# # 
+# # dmy <- dummyVars(" ~ .", data = df)
+# # trsf <- data.frame(predict(dmy, newdata = df))
+# 
+# Split data into train and test
+# set.seed(123)
+# sample.split = sample(c(TRUE,FALSE), nrow(df), replace = TRUE, prob=c(0.8,0.2))
+# 
+# # Remvoe last sample int since I said so
+# #sample.split = sample.split[-which(sample.split == nrow(df))]
+# 
+# train = df[sample.split,]
+# test = df[!sample.split,]
+# 
+# train = as.matrix(train)
+# test = as.matrix(test)
+# Symbol = 'ETHUSD'
+# Timeframe = '1D'
+# TargetIncreasePercent = "4"
+df = readRDS(paste0("bsts/df_",Symbol,Timeframe,".rds"))
+sample.split = readRDS(paste0("bsts/sample.split_",Symbol,Timeframe,TargetIncreasePercent,".rds"))
+outcome = readRDS(paste0("bsts/outcome_",Symbol,Timeframe,TargetIncreasePercent,".rds"))
+test = readRDS(paste0("bsts/test_",Symbol,Timeframe,TargetIncreasePercent,".rds"))
+train = readRDS(paste0("bsts/train_",Symbol,Timeframe,TargetIncreasePercent,".rds"))
+
+outcome.train = outcome[sample.split]
+outcome.test = outcome[!sample.split]
+
+
+assign('train',train,.GlobalEnv)
+
+# Created boosted model
+# bst = xgboost(data = train,
+#               label = outcome.train,
+#               objective = "binary:logistic",
+#               max.depth = 10,
+#               nrounds = 50)
+bst = readRDS(paste0("bsts/bst_",Symbol,Timeframe,TargetIncreasePercent,".rds"))
+# Predict
+predictions = predict(bst, test)
+Actual.Percent.High = round((((df$High / df$Open) * 100) - 100), digits = 1)
+Actual.Percent.Close = round((((df$Close / df$Open) * 100) - 100), digits = 1)
+Actual.Percent.Low = round((((df$Low / df$Open) * 100) - 100), digits = 1)
+compare = data.frame("Actual" = outcome.test,
+                     "Actual.Percent.High" = Actual.Percent.High[which(!sample.split) + 1],
+                     "Actual.Percent.Low" = Actual.Percent.Low[which(!sample.split) + 1],
+                     "Actual.Percent.Close" = Actual.Percent.Close[which(!sample.split) + 1],
+                     "Probability" = round(predictions, digits = 4),
+                     "Prediction" = NA)
+compare$Prediction[compare$Probability >= SuccessThreshold] = 1
+compare$Prediction[compare$Probability < SuccessThreshold] = 0
+
+accuracy = length(which(compare$Actual == compare$Prediction)) / nrow(compare) * 100
+print(accuracy)
+
+
+if(TP == 0 & SL == 0){
+  examine = compare[compare$Prediction == 1, ]
+  accuracy2 = sum(as.numeric(as.character(examine$Actual.Percent.Close)))
+  print(accuracy2)
+}else{
+
+  examine = compare[compare$Prediction == 1, ]
+  winning.trades = examine[examine$Actual == 1,]
+  winning.trades$Actual.Percent.High[winning.trades$Actual.Percent.High > TP ] = TP
+  winning.trades.above = winning.trades[winning.trades$Actual.Percent.High == TP,]
+  winning.trades.below = winning.trades[winning.trades$Actual.Percent.High < TP,]
+  winning.sum.below = sum(as.numeric(as.character(winning.trades.below$Actual.Percent.Close)))
+  winning.sum.above = sum(as.numeric(as.character(winning.trades.above$Actual.Percent.High)))
+  winning.sum = winning.sum.above + winning.sum.below
+  missed.trades = examine[examine$Actual == 0,]
+  # missed.trades$Actual.Percent.Close[missed.trades$Actual.Percent.Close < SL] = SL
+  missed.sum = sum(as.numeric(as.character(missed.trades$Actual.Percent.Close)))
+  accuracy2 = winning.sum + missed.sum
+  # accuracy2 = sum(as.numeric(as.character(examine$Actual.Percent.Close)))
+  print(accuracy2)
+}
+
+
+yes.buy = compare[compare$Prediction == 1, ]
+yes.buy.correct.perc = length(which(yes.buy$Prediction == yes.buy$Actual)) / nrow(yes.buy) * 100
+
+no.buy = compare[compare$Prediction == 0, ]
+no.buy.correct.perc = length(which(no.buy$Prediction == no.buy$Actual)) / nrow(no.buy) * 100
+
+
+assign('yes.buy.correct.perc',yes.buy.correct.perc,.GlobalEnv)
+assign("no.buy.correct.perc",no.buy.correct.perc,.GlobalEnv)
+assign("overall.accuracy",accuracy,.GlobalEnv)
+assign("compare",compare,.GlobalEnv)
+assign("sum.percentage",accuracy2,.GlobalEnv)
+assign('bst',bst,.GlobalEnv)
+}
+
+# Predict Best
+predict.best <- function(SuccessThreshold = 0.3, all.bst, all.bst.names){
+  all.predictions = c()
+  
+  for(j in 1:length(all.bst)){
+    
+    
+    bst = all.bst[[j]]
+    names = all.bst.names[j]
+    
+    current = getQuote("DOGE-USD")
+    current = current[,-c(3,8)]
+    current = current[,c(1,2,4:6,3)]
+    colnames(current) = c("Date","Close","Open","High","Low","Percent.Change")
+    #if(df2$DOGE.USD.Close[1] < df2$DOGE.USD.Close[2]){
+    #  current$Previous = 1
+    #}else if(df2$DOGE.USD.Close[1] > df2$DOGE.USD.Close[2]){
+    #  current$Previous = 0
+    #}
+    
+    # Format it the same as the model
+    current$Date = as.Date(current$Date)
+    current$Date = str_match(string = current$Date, pattern = "-(.*)")[,2]
+    current$Date = str_replace(current$Date, pattern = "-.*", replacement = "")
+    current$Date = as.numeric(current$Date)
+    
+    current = as.matrix(current)
+    
+    predict.now = predict(bst, current)
+    if(predict.now >= SuccessThreshold){
+      print("buy")
+    }else{
+      print("not good")
+    }
+    
+    all.predictions = c(all.predictions, predict.now)
+    
+    assign("all.predictions",all.predictions,.GlobalEnv)
+    assign(paste0("predict_",names) ,predict.now,.GlobalEnv)
+  }
+}
+
+predict.tomorrow <- function(SuccessThreshold, Symbol){
+
+  current = getQuote(Symbol)
+  current = current[,-c(3,8)]
+  current = current[,c(1,2,4:6,3)]
+  colnames(current) = c("Date","Close","Open","High","Low","Percent.Change")
+  # if(df2$DOGE.USD.Close[1] < df2$DOGE.USD.Close[2]){
+  #  current$Previous = 1
+  # }else if(df2$DOGE.USD.Close[1] > df2$DOGE.USD.Close[2]){
+  #  current$Previous = 0
+  # }
+  
+  # Format it the same as the model
+  current$Date = as.Date(current$Date)
+  current$Date = str_match(string = current$Date, pattern = "-(.*)")[,2]
+  current$Date = str_replace(current$Date, pattern = "-.*", replacement = "")
+  current$Date = as.numeric(current$Date)
+  
+  current = as.matrix(current)
+  assign('current',current,.GlobalEnv)
+  
+  predict.now = predict(bst, current)
+  if(predict.now >= SuccessThreshold){
+    print("buy")
+  }else{
+    print("not good")
+  }
+  
+  assign(paste0("predict.now") ,predict.now,.GlobalEnv)
+}
+
+predict.tomorrow.multiple <- function(Symbols, Timeframe, SuccessThreshold){
+  # Symbols = Symbols
+  # Symbols = c('ethusd','btcusd')
+  # Timeframe = '7day'
+  predictions.df.comb = data.frame("Coin" = character(),
+                              "Price Change" = character(),
+                              "Probability" = character(),
+                              "Confidence Score" = character())  
+  
+  for(i in 1:length(Symbols)){
+
+    if(Timeframe == '4hour'){
+      df = riingo_crypto_latest(Symbols[i], resample_frequency = Timeframe)
+      if(nrow(df) == 1){
+        df = riingo_crypto_prices(Symbols[i], end_date = Sys.Date(), resample_frequency = Timeframe)
+      }
+    }else{
+      df = riingo_crypto_prices(Symbols[i], end_date = Sys.Date(), resample_frequency = Timeframe)
+    }
+    
+    # Modify data to be more useable
+    df$Percent.Change = NA
+    df = df[-1,-c(1:3,9:11)]
+    colnames(df) = c("Date","Open","High","Low","Close","Percent.Change")
+    df$Percent.Change = round((((df$Close / df$Open) * 100) - 100), digits = 1)
+    
+    # Add column for binary previouos day change
+    df$Previous = NA
+    for(k in 2:nrow(df)){
+      if(df$Percent.Change[k - 1] <= 0){
+        df$Previous[k] = 0
+      }else{
+        df$Previous[k] = 1
+      }
+    }
+    
+    # We're only interested in the most recent completed row
+      df = df[nrow(df)-1,]
+
+    
+    # Round columns to be more general
+    df$Close = round(df$Close, digits = 3)
+    df$Open = round(df$Open, digits = 3)
+    df$High = round(df$High, digits = 3)
+    df$Low = round(df$Low, digits = 3)
+    
+    
+    # Convert to actual dates and remove year and change to numeric
+    df$Date = as.Date(df$Date)
+    df$Date = str_match(string = df$Date, pattern = "-(.*)")[,2]
+    df$Date = str_replace(df$Date, pattern = "-.*", replacement = "")
+    df$Date = as.numeric(df$Date)
+    
+    
+    predictions.df = data.frame("Coin" = toupper(Symbols[i]),
+                                "Price Change" = 1:20,
+                                "Probability" = 0,
+                                "Confidence Score" = NA)
+    
+    predictions = c()
+    for(j in 1:20){
+      bst = readRDS(paste0('bsts/bst_',toupper(Symbols[i]),Timeframe,j,'.rds'))
+      df = as.matrix(df)
+      predict.next = predict(bst, df)
+      predictions = c(predictions,predict.next)
+    }
+    predictions.df$Price.Change = paste0(predictions.df$Price.Change,"%")
+    predictions.df$Confidence.Score = predictions
+    predictions.df$Probability[predictions.df$Confidence.Score >= SuccessThreshold] = 1
+
+    predictions.df.comb = rbind(predictions.df.comb,predictions.df)
+
+  }
+  predictions.df.comb$Confidence.Score = round(predictions.df.comb$Confidence.Score, digits = 4)
+  assign("predictions.df.comb",predictions.df.comb,.GlobalEnv)
+  
+  
+  
+
+  
+}
---
+++ app.R
@@ -0,0 +1,224 @@
+library(shiny)
+library(shinydashboard)
+library(dashboardthemes)
+library(stringr)
+library(ggplot2)
+library(DT)
+
+# Define UI
+ui <- dashboardPage(
+  # skin = "purple",
+  dashboardHeader(title = "Crypto ML"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Creating a Model", tabName = "create"),
+      #menuItem("Predict Tomorrow", tabName = "predict"),
+      menuItem("Predict Next Candle (Multiple)", tabName = 'predictMultiple')
+      # menuItem("Most Likely Outcome", tabName = "likely")
+      
+    )
+  ),
+  dashboardBody(
+    shinyDashboardThemes(
+      theme = "poor_mans_flatly"
+    ),
+    tabItems(
+      tabItem(tabName = "create",
+              fluidRow(
+                strong(h2("Creating a Model:")),
+                paste0("On this tab you can use the sliders to modify how the predictive model is created. First you need to select a timeframe ",
+                       "and coin that you're interested in predicting. ","The first slider is used ",
+                       "to select the percentage increase in the timeframe that you've selected. The second slider is used ",
+                       "to select how confident you want the model to be in order to classify a 'hit'. The model will make a prediction on a scale from ",
+                       "0-1, the closer to 1 the prediction is, the more confident the model is that your selected percentage increase will happen in your selected timeframe."),
+                br(),
+                br(),
+                selectInput("timeframe","Pick a Timeframe", choices = list("4 Hour" = "4hour",
+                                                                           "1 Day" = "1day",
+                                                                           "1 Week" = "7day")),
+                selectInput("select","Pick a stock to predict", choices = list("Ethereum" = "ETHUSD",
+                                                                               "BitCoin" = "BTCUSD")),
+                column(width = 6,
+                       box(title = "Inputs", solidHeader = TRUE, status = "primary", width = NULL,
+                         strong("Note if you're using a mobile device you can just tap the number on the slider instead of actually sliding it."),
+                         br(),
+                         br(),
+                         sliderInput("slider1","Select Percentage Increase", min = 1, max = 50, step = 1, value = 4),
+                         sliderInput("slider2", "Select Prediction 'hit' Threshold", min = 0.1, max = 1, step = 0.05, value = 0.3),
+                         strong("Note:"),
+                         paste0("Metrics by default are calculated based on the candles closing value. ",
+                                "You can use the TP (take profit) and SL (stop loss) input fields to specify your TP/SL. ",
+                                "Setting a TP/SL can limit your gains, but can also limit your losses! ",
+                                "Leaving both the TP/SL values at 0 will set the metrics to be calculated based on candles closing value."),
+                         numericInput('tp',"Set TP (must be positive)", value = 0, min = 0),
+                         numericInput("sl","Set SL (must be negative)", value = 0, max = 0),
+                         actionButton('action1', label = "Generate"),
+                         br(),
+                         br(),
+                       ),
+                       box(title = "Metrics", width = NULL, status = "primary", solidHeader = TRUE,
+                         infoBoxOutput("OverallAccuracy", width = 6),
+                         infoBoxOutput("SumPercentage", width = 6),
+                         infoBoxOutput("Buy", width = 6),
+                         infoBoxOutput("DontBuy", width = 6),
+                         infoBoxOutput("Predictions", width = 6),
+                         infoBoxOutput("Hits", width = 6)
+                         
+                       ),
+                       box(title = "Histogram", width = NULL, status = "primary", solidHeader = TRUE,
+                         paste0("Ideally, we'd like there to be a near 0 probability or a near 1 probability for all predictions. ",
+                                "Values that are more in the middle can give us an unclear prediction. Since it's rare that the stock ",
+                                "market will give a clear indicator of an increase over the next 24hrs, it's usually useful to set the ",
+                                "prediction 'hit' threshold somewhat low."),
+                         plotOutput("modelPlot")
+                       )
+                ),
+                column(width = 6,
+                box(width = NULL, title = "Backtest", status = "primary", solidHeader = TRUE,
+                  dataTableOutput("table1")
+                )
+              )
+              )
+      ),
+      # tabItem(tabName = "predict",
+      #         fluidRow(
+      #           strong(h3("About:")),
+      #           strong("Note that you must create a model on the previous tab before predicting tomorrow!"),
+      #           br(),
+      #           paste0("This tab will simply generate a prediction using the model you created in the 'Creating a Model' Tab. ",
+      #                  "Remember that the probability is on a scale of 0-1, where a larger value represents a higher probability of your ",
+      #                  "model's prediction coming true."),
+      #           br(),
+      #           br(),
+      #           box(title = "Predict Tomorrow", status = "primary", solidHeader = TRUE,
+      #             textInput("open","Open"),
+      #             textInput("close","Close"),
+      #             textInput("low","Low"),
+      #             textInput("high","High"),
+      #             actionButton("action2","Predict"),
+      #             br(),
+      #             br(),
+      #             infoBoxOutput("predict", width = NULL)
+      #           )
+      #         )
+      #         ),
+      tabItem(tabName = "predictMultiple",
+              fluidRow(
+                strong(h3("About:")),
+                paste0("On this tab you can generate predictions for multiple coins! Simply use the check boxes to select which coins you'd like to predict.",
+                       " If you'd like to export these results, simply press the 'csv' button on top of the table below."),
+                br(),
+                br(),
+                box(title = "Predict Multiple", status = "primary", solidHeader = TRUE,
+                    checkboxGroupInput('checkGroup', label = 'Select Coin(s)',
+                                       choices = list("Bitcoin" = 'btcusd', "Ethereum" = 'ethusd'),
+                                       selected = 'btcusd'),
+                    selectInput("timeframePredict","Pick a Timeframe", choices = list("4 Hour" = "4hour",
+                                                                                      "1 Day" = "1day",
+                                                                                      "1 Week" = "7day")),
+                    sliderInput("slider3", "Select Prediction 'hit' Threshold", min = 0.1, max = 1, step = 0.05, value = 0.3),
+                    actionButton("action4","Predict"),
+                    br(),
+                    br()
+                    
+
+                ),
+                dataTableOutput("multipleOutput")
+              )
+      )
+      # tabItem(tabName = "likely",
+      #         fluidRow(
+      #           strong(h3("About:")),
+      #           paste0("This tab will run models with a target 24hr percentage increase from -5 to 10. Each success threshold is set ",
+      #                  "to a probability value of 0.3. The most probable prediction will then be displayed."),
+      #           br(),
+      #           br(),
+      #           box(
+      #             actionButton("action3","Predict Most Likely"),
+      #             br(),
+      #             br(),
+      #             strong("Most Probable Outcome:"),
+      #             textOutput("mostLikely"),
+      #             textOutput("percentChance")
+      #           )
+      #         )
+      # )
+    )
+  )
+  
+  
+)
+
+# Define server logic
+server <- function(input, output) {
+
+  # Read in functions
+  source("DogeCoinML.R")
+  
+  observeEvent(input$action3, {
+    showModal(modalDialog("Predicting Most Likely...", footer = NULL))
+    on.exit(removeModal())
+    all.bst.names = list.files(path = "bsts", pattern = ".rds")
+    all.bst.numbers = str_match(string = all.bst.names, pattern = "bst_(.*)\\.")[,2]
+    all.bst.path = list.files(path = "bsts", pattern = ".rds", full.names = TRUE)
+    all.bst = lapply(all.bst.path, readRDS)
+    assign('all.bst.numbers',all.bst.numbers,.GlobalEnv)
+    assign('all.bst',all.bst,.GlobalEnv)
+    
+    predict.best(0.3, all.bst, all.bst.names)
+    
+    all.predictions = round(all.predictions, digits = 4)
+    max.pred = which(all.predictions == max(all.predictions))
+    max.bst = all.bst.numbers[max.pred]
+    
+    output$mostLikely = renderText(paste0("The most probable outcome over the next 24 hours is a change of ",max.bst,"% or more."))
+    output$percentChance = renderText(paste0(max(all.predictions)," Probability Predicted"))
+  })
+
+  
+  observeEvent(input$action1, {
+    showModal(modalDialog("Generating Your Model...", footer = NULL))
+    on.exit(removeModal())
+    createModel(input$slider1, input$slider2, input$select, input$timeframe, input$tp, input$sl)
+    output$OverallAccuracy = renderInfoBox({
+      infoBox("Overall Accuracy",paste0(round(overall.accuracy, digits = 2), "%"), icon = icon('check'))
+      })
+    output$SumPercentage = renderInfoBox({
+      infoBox("Sum Percentage", paste0(round(sum.percentage, digits = 2), "%"),icon = icon("money-bill-trend-up"))
+      })
+    output$Buy = renderInfoBox({infoBox("'Buy' Correct", paste0(round(yes.buy.correct.perc, digits = 2), "%"), icon = icon("thumbs-up"))
+      })
+    output$DontBuy = renderInfoBox({infoBox("'Don't Buy' Correct", paste0(round(no.buy.correct.perc, digits = 2),"%"),icon = icon("thumbs-down"))
+      })
+    output$Predictions = renderInfoBox({infoBox("'Number of Predictions", paste0(nrow(compare)))
+    })
+    output$Hits = renderInfoBox({infoBox("'Number of 'Hits'", paste0(nrow(compare[compare$Prediction == 1,])))
+    })
+    
+    output$table1 = renderDataTable(compare, rownames = FALSE, options = list(pageLength = 20))
+    output$modelPlot = renderPlot(hist(compare$Probability))
+    output$modelPlot = renderPlot(ggplot(data = compare, aes(x = Probability)) + geom_histogram(colour = "blue", alpha = 0.3))
+  })
+  
+  observeEvent(input$action2, {
+    predict.tomorrow(0.3, input$select)
+    output$textToday = renderText(paste0("Probability of: ",round(predict.now, digits = 4)))
+    output$predict = renderInfoBox({
+      infoBox("Predicted Probability",round(predict.now, digits = 4))
+    })
+  })
+  
+  observeEvent(input$action4, {
+    showModal(modalDialog("Generating predictions...", footer = NULL))
+    on.exit(removeModal())
+    predict.tomorrow.multiple(input$checkGroup, input$timeframePredict, input$slider3)
+    output$multipleOutput = renderDataTable(predictions.df.comb,
+                                            rownames = FALSE,
+                                            extensions = "Buttons",
+                                            options = list(paging = FALSE, searching = FALSE, dom = 'Bfrtip', buttons = c('csv')))
+  })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
