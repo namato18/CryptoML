8a92385164f86a6826ae11cdd5ddb7d61f8c3482
michael j Swallower <nickamato608@gmail.com>
2023-05-22 21:02:53 -0600

    Added .Renviron and push app file

---
+++ push_app.R
@@ -0,0 +1,7 @@
+library(rsconnect)
+
+# rsconnect::deployApp("C:/Users/xbox/Desktop/Rstuff/WoWPvPTracker")
+# 
+# rsconnect::deployApp("C:/Users/xbox/Desktop/Rstuff/NecessaryPoll")
+
+rsconnect::deployApp("C:/Users/xbox/Desktop/Rstuff/CryptoML")
